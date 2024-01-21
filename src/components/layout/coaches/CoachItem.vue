4817b02b24530186f779a53ca756e935979901bc
SabaMuladze <muladze66@bk.ru>
2024-01-21 15:22:53 +0400

    add and develop CoachItem components

---
+++ src/components/layout/coaches/CoachItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
+    <div>
+      <span v-for="area in areas" :key="area">{{ area }}</span>
+    </div>
+    <div class="actions">
+      <router-link :to="contantLink">Contact</router-link>
+      <router-link :to="viewDetsLink">View Details</router-link>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["fullName", "areas", "rate", "id"],
+  computed: {
+    contantLink() {
+      return "/coaches/" + this.id + "/contact";
+    },
+    viewDetsLink() {
+      return "/coaches/" + this.id;
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
