5ec23f0d4a0ca547bcd15b3190dd3838f236dfcb
SabaMuladze <muladze66@bk.ru>
2024-01-21 22:59:17 +0400

    registration in progress

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -1,11 +1,20 @@
-<template>Coach Registration</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a Coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
 
 <script>
+import CoachForm from "../../components/coaches/CoachForm.vue";
 export default {
   data() {
-    return {
-      message: "World",
-    };
+    return {};
+  },
+  components: {
+    CoachForm,
   },
 };
 </script>
